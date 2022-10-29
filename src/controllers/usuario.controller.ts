cbd6b7e2ee07758c4844e4d35856a7a656c088b0
OrlandoGue <orlandook283@gmail.com>
2022-10-28 20:14:14 -0500

    Cambios 28 de Octubre

--- src/controllers/usuario.controller.ts
+++ src/controllers/usuario.controller.ts
@@ -23,6 +23,11 @@ import {service} from '@loopback/core';
 import {AuthService} from '../services';
 import axios from 'axios';
 import {configuracion} from '../config/config';
+import {Credenciales} from '../models';
+import { HttpErrors} from '@loopback/rest';
+import {authenticate} from '@loopback/authentication';
+
+@authenticate("admin")
 export class UsuarioController {
   constructor(
     @repository(UsuarioRepository)
@@ -30,7 +35,37 @@ export class UsuarioController {
     @service(AuthService)
     public servicioAuth: AuthService
     ) {}
+//Servicio de login
+@authenticate.skip()
+@post('/login', {
+  responses: {
+    '200': {
+      description: 'Identificación de usuarios'
+    }
+  }
+})
+async login(
+  @requestBody() credenciales: Credenciales
+) {
+  let p = await this.servicioAuth.identificarPersona(credenciales.usuario, credenciales.password);
+  if (p) {
+    let token = this.servicioAuth.GenerarTokenJWT(p);
 
+    return {
+      status: "success",
+      data: {
+        nombre: p.Nombre,
+        apellidos: p.Apellidos,
+        correo: p.Correo,
+        id: p.id
+      },
+      token: token
+    }
+  } else {
+    throw new HttpErrors[401]("Datos invalidos")
+  }
+}
+  @authenticate.skip()
   @post('/usuarios')
   @response(200, {
     description: 'Usuario model instance',
